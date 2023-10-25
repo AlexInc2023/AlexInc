e96621b87710830c211d13a4d8dabdec77949ca8
BrianWanamaker <bcwanamaker@quinnipiac.edu>
2023-10-25 19:15:01 -0400

    fixed news letter modal

--- js/init.js
+++ js/init.js
@@ -111,28 +111,36 @@ document.querySelectorAll('.item-wrap a').forEach(function (link) {
     });
 });
 
-// Get the modal
-var modal = document.getElementById('newsletterModal');
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the modal
+    var modal = document.getElementById('newsletterModal');
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName('close-btn')[0];
+    // Check if modal exists
+    if (!modal) return;
 
-// Show the modal when the page loads
-window.onload = function () {
-    modal.style.display = "block";
-}
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName('close-btn')[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    // Show the modal when the page loads
+    window.onload = function () {
+        modal.style.display = "block";
     }
-}
+
+    // When the user clicks on <span> (x), close the modal
+    if (span) {
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
+
 
 
 
